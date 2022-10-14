faa0767afad96afb578f959aedce91b52a7f74cf
mostafizur99 <coder.mr11@gmail.com>
2022-10-14 23:28:50 +0600

    feat: react-router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1> hello Londogen root</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>
+        <h1>  About</h1>
+        <Link to="/">Home</Link>
+      </div>
+    },
+  ])
+
   return (
     <div >
-      hello Londogen
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
