2cf00b0b41ab7d7e1b38a928129dbb99bf553d02
mostafizur99 <coder.mr11@gmail.com>
2022-10-20 07:14:06 +0600

    feat: home category data-loader

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       children: [
         {
           path: "/",
+          loader: () => fetch('categoryDb.json'),
           element: <Home />,
         },
         {
--- src/components/Homes/Home.js
+++ src/components/Homes/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const categoryData = useLoaderData();
+
     return (
         <div className='container mx-auto'>
             Home Page
+
+            <div className='grid grid-cols-4 py-10'>
+                {
+                    categoryData &&
+                    categoryData.map(category => (
+                        <div key={category._id} className='mb-3'>
+                            <img src={category.picture} alt="category" />
+                            <h2>{category.title} </h2>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
